157b53090bdc5f6eb60393f089cbd65d90fc2321
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-06-20 16:36:15 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// Hero Object
+const superHero = {
+  name: "Batman",
+  secretIdentity: "Bruce Wayne",
+  powers: [
+    "Superhuman Intelligence",
+    "Master Martial Artist",
+    "Enhanced Strength",
+    "Ehanced Reflexes",
+    "Master of Disguise",
+  ],
+  weakness: "Lack of superhuman abilities",
+};
